87162bd495da7493ff071eb3830542c6f6b0eee1
Itay Moav <itay.malimovka@gmail.com>
2014-09-25 13:31:58 -0400

    using lilpy
    and modifying some file names

--- pyverse/rahl.py
+++ pyverse/rahl.py
@@ -5,17 +5,16 @@
 import sys
 import os
 import fnmatch
-import sp_data
-
+import AutoComplete
 
 
 PATH = '\\'
 matches = []
-for root, dirnames, filenames in os.walk('app_db'):
+for root, dirnames, filenames in os.walk('assets/sp/dhara'):
 	for filename in fnmatch.filter(filenames, '*.sql'):
 		#print("root [{}]  dirname [{}] file [{}]\n\n".format(root,dirnames,filename))
 		matches.append(root + PATH + filename)
-	 
+
 # print("Parsing the following files" + "\n".join(matches))
 
 # open the doc file
@@ -34,20 +33,20 @@ to_break = False #for debug purposes, to later break after a specific SP
 
 for fn in matches:
 	# print(fn)
-	
-	
+
+
 	sp_file = open(fn,'r')
-	
+
 	looking_for_header		= True
 	looking_for_header_args = False
 	looking_for_body   		= False
 	not_yet_started_args	= True #once I start looking into the args string, I no longer start from (, as it can be the ( in INT(11)
-	SP						= sp_data.SpData(fn)
-	
+	SP						= AutoComplete.SpData(fn)
+
 	for line in sp_file:
 		if looking_for_body:
 			SP.addBodyLine(line)
-	
+
 		if looking_for_header:
 			# method name
 			if "CREATE PROCEDURE" in line and len(line)>(len("CREATE PROCEDURE")+8):
@@ -57,49 +56,49 @@ for fn in matches:
 				# TODO move this into the addSPName : function_header = "\tpublic function %s("%func_name.replace('`','').strip()
 				looking_for_header = False
 				looking_for_header_args = True
-				
+
 		# method arguments
 		if looking_for_header_args:
 			if not_yet_started_args:
 				start_args	= line.find('(')
 				not_yet_started_args = False
-				
+
 			else:
 				start_args  = 0
 			end_args	= line.find('BEGIN')
-			
+
 			if(end_args > -1): # Means we got to the end of the args section
 				looking_for_header_args = False
 				looking_for_body = True
-			
+
 			SP.addRawArgStr(line[start_args+1:])
 			# clean_args += '$' + argstr.replace('IN ','').replace('OUT ','').replace(',',', $').replace('$ ','$')
-	
-	
+
+
 
 	#print phpdoc
 	doc_file.write(str(SP))
-	
+
 	#print the function header
 	# doc_file.write(function_header)
-	
+
 	#print the function argument part
 	# doc_file.write(clean_args)
-			
-			
+
+
 	#print(SP.sp_name)
 	#print(SP.raw_args)
-			
-			
-		
+
+
+
 	sp_file.close()
 	if to_break:
 		break
-	
+
 	# for debug, enter SP file name + path u want to break one SP after...
 	#if fn == 'app_db\Course\Wizard\scheduler_dates.sql':
 	#	to_break = True
-		
+
 # doc_file.write("\n\n}\n")
 # doc_file.close()
 
