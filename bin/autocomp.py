1f3941e5349c1ef19699f54040799c9a55bf969a
Itay Moav <itay.malimovka@gmail.com>
2015-02-24 14:50:47 -0500

    assets dynamic load

--- bin/autocomp.py
+++ bin/autocomp.py
@@ -28,6 +28,7 @@ def main():
         parser.add_argument("--all", dest="handle_all", action="store_true", help="Specifying this flag will generate SP auto completion for all databases")
         parser.add_argument("-v", "--verbose", dest="verbosity", action="store_true", help="Specifying this flag will echo list of files processed")
         parser.add_argument("-d","--database", dest="database", action="store",nargs='?', default=False, const='All', help="Generate php auto complete file for stored procedures, or the folder/*.sql specified. Root folder is the database name.")
+        parser.add_argument("-a", "--assets", dest="assets_path", action="store", nargs='?',  help="optional way to specifiy the assets full path (starting from /)")
 
         Builder = app.autocompletion.SP(parser)
         Builder.run()
