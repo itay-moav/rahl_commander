61d14cac9576e933e2d61f732af0f3f81cef1029
Itay Moav <itay.malimovka@gmail.com>
2016-01-26 08:33:29 -0500

    autocomplete config

---
+++ example_config/autocomplete.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 5, 2016
+
+@author: Itay Moav
+
+config for the autocompletion component
+Right now we support only PHP + Eclipse
+so all is implicitly for this combo.
+
+1. Return type from stored procedure (will translate in the PHPDoc commnets to @return [Return_Type]
+2. Location of the Eclipse installation (by which way it will know where to put the SP.php file created. If it can't write to this directory,
+   it will write it to the assests folder under the autocompletion folder / php /eclipse
+'''
+
+autocomplete = {   'language':         'php',      \
+                   'editor':           'eclipse',  \
+                   'editor_workspace': '/path/to/eclipse/workspace/you/want/autocomplete/for LEAVE EMPTY IF NO AUTO COPY IS DESIRED',    \
+                   'return':           'Return_Data_Type',    \
+                                                              \
+                   'eclipse':          {     \
+                       'plugin_dir':  '.metadata/.plugins/org.eclipse.php.core/__language__'    \
+                   }    \
+} 
