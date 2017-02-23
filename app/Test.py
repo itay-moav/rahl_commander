5f73b515e503151b778ff536ff254a099b89e3ff
Itay Moav <itay.malimovka@gmail.com>
2017-02-23 13:43:13 -0500

    testing schema and scripts too

--- app/Test.py
+++ app/Test.py
@@ -8,7 +8,6 @@ import os
 import config
 from app import logging as L
 
-
 class Install(app.iterator.AssetFilesDBConn):
 
 
@@ -16,31 +15,43 @@ class Install(app.iterator.AssetFilesDBConn):
         '''
         Main iteration processor bala bala
         '''
+        # NOTICE! assets folder is not tested for as there is an intrinsic test for it in the core system, it will throw an EnvironmentException
 
+        
         # First check the auto completion folder exists
         if not os.path.isdir(config.assets_folder + '/autocompletion'):
             print("WARNING: OH MY GOD!  XX You are missing autocompletion folder [{}]. Please create it and run tests again".format(config.assets_folder + '/autocompletion'))
         else: 
-            print("GOOD! autocompletion folder found [{}]".format(config.assets_folder + '/autocompletion'))
+            print(" GOOD! autocompletion folder found [{}]".format(config.assets_folder + '/autocompletion'))
 
         if not os.path.isdir(config.assets_folder + '/autocompletion/php'):
             print("WARNING: OH MY GOD!  XX You are missing autocompletion folder for PHP [{}] folder FOR PHP. Create it if you plan to use it".format(config.assets_folder + '/autocompletion/php'))
         else:
-            print("GOOD!  autocompletion folder found for PHP [{}].".format(config.assets_folder + '/autocompletion/php'))
+            print(" GOOD! autocompletion folder found for PHP [{}].".format(config.assets_folder + '/autocompletion/php'))
+
+        if not os.path.isdir(config.assets_folder + '/scripts'):
+            print("WARNING: OH MY GOD!  XX You are missing scripts folder [{}]. Please create it if u need it, and run tests again".format(config.assets_folder + '/scripts'))
+        else: 
+            print(" GOOD! scripts folder found [{}]".format(config.assets_folder + '/scripts'))
+            self.folders.append(self.assets_path + '/scripts')
+        
+        if not os.path.isdir(config.assets_folder + '/schema'):
+            print("WARNING: OH MY GOD!  XX You are missing Schema Checker folder [{}]. Please create it if u need it, and run tests again".format(config.assets_folder + '/schema'))
+        else: 
+            print(" GOOD! Schema Checker folder found [{}]".format(config.assets_folder + '/schema'))
+            self.folders.append(self.assets_path + '/schema')
 
         # Check the db objects folder exist
-        sub_filders_to_check = []
         for sub_folder in self.folders:
             #check subfolder exists or fail
             if not os.path.isdir(sub_folder):
                 print("ERROR: You are missing [{}] folder. Please create it and run tests again".format(sub_folder))
             else:
-                print("GOOD! folder [{}] was found".format(sub_folder))
-                sub_filders_to_check.append(sub_folder)
+                print(" GOOD! folder [{}] was found".format(sub_folder))
 
         # CHECK DBs existance
         print("\n\nNOW! Now I will test all dbs you try to work with exists\n\n")
-        for sub_folder in sub_filders_to_check:
+        for sub_folder in self.folders:
         
             # Loop on files and run sql
             for root, dirnames, filenames in os.walk(sub_folder):
