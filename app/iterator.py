8f1140bc8f9b92f73ea58ca516d8126b847cf868
Itay Moav <itay.malimovka@gmail.com>
2017-08-10 16:56:15 -0400

    ALLOW for db coneccion injection via ARGS

--- app/iterator.py
+++ app/iterator.py
@@ -201,8 +201,13 @@ class AssetFilesDBConn(AssetFiles):
         '''
             overwrite this, if no DB connection is needed
         '''
-        self.cnx = app.db.get_connection()
-        self.cursor = self.cnx.cursor()
+        #Allow for dependency injection of the connection
+        # This will allow the use of test server in conjunction with the actual server
+        if self.args.cnx: 
+            self.cnx = self.args.cnx
+        else: 
+            self.cnx = app.db.get_connection()
+            self.cursor = self.cnx.cursor()
 
 
     def changeDB(self,db,file_content):
