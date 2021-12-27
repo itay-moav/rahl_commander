a66d3a68ab29128f63052b7806dba251d7936f9c
itay-moav <itay.malimovka@gmail.com>
2021-12-27 15:10:32 -0500

    figuring out globals and profiles

---
+++ example_environment/autocomplete.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 5, 2016
+
+@author: Itay Moav
+
+config for the autocompletion component
+Right now we support only PHP + Eclipse
+so all is implicitly for this combo.
+
+1. [return] Return type from stored procedure (will translate in the PHPDoc commnets to @return [Return_Type]
+2. [db_name_separator]  When the auto completer will created the stored procedure pseudo php code, it will name the methods db_name[db_name_separator]stored_procedue_name
+'''
+
+
+autocomplete = {   'language':          'php',      \
+                   'return':            '\SP',      \
+                   'db_name_separator': '__'        \
+                   }    \
---
+++ example_environment/ignore_list.py
@@ -0,0 +1,9 @@
+'''
+Created on Jan 15, 2015
+
+@author: Itay Moav
+
+Here add subfolders in the ASSETS folder to be ignored by
+RAHL COMMANDER commands
+'''
+ignore_files_dirs_with = ['.svn','.md']
